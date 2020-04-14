50f40fd594a2fdaa40872184e87b974304a7bc41
Ronald Fornis Paglinawan <ronaldpaglinawan@yahoo.com>
2020-04-15 10:43:52 +1200

    Add project files.

---
+++ ParkyAPI/Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,22 @@
+﻿using ParkyAPI.Models;
+using ParkyAPI.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository.IRepository
+{
+    public interface INationalParkRepository
+    {
+        ICollection<NationalParkDTO> GetNationalParks();
+
+        NationalParkDTO GetNationalPark(int nationalParkId);
+        bool NationalParkExists(string name);
+        bool NationalParkExists(int id);
+        bool CreateNationalPark(NationalParkDTO nationalPark);
+        bool UpdateNationalPark(NationalParkDTO nationalPark);
+        bool DeleteNationalPark(NationalParkDTO nationalPark);
+        bool Save();
+    }
+}
